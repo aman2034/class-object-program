7e286797d0acf85de00149147aa54b5634c280d0
aman2034 <amankr2034@gmail.com>
2025-04-03 15:13:01 +0530

    Add files via upload

---
+++ class_object.cpp
@@ -0,0 +1,24 @@
+//LAB:-1 ( AREA OF RECTANGLE)
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+    private:
+    int length, width;
+    public:
+    Rectangle(int l, int w)
+    {
+        length=l;
+        width=w;
+    }
+        int area()
+        {
+            return length*width;
+        }
+};
+main()
+{
+    Rectangle rect(3,5);
+    cout<<rect.area()<<endl;
+    return 0;
+}
